dc1c9a578af2b55099ed984ace3fa47773d7551f
pikesley <github@orgraphone.org>
2015-06-29 17:09:17 +0100

    Move attributes out of .kitchen.yml

---
+++ attributes/default.rb
@@ -0,0 +1,20 @@
+default['user'] = 'certificates'
+default['fully_qualified_domain_name'] = 'certificates.theodi.org'
+default['catch_and_redirect'] = ['certificate.theodi.org']
+default['start_port'] = 8000
+default['concurrency'] = 2
+default['repo'] = 'theodi/open-data-certificate'
+
+default['mysql']['database'] = 'certificates'
+default['migrate'] = 'bundle exec rake db:schema:load'
+default['post_deploy_tasks'] = ['bundle exec rake odc:deploy']
+default['deployment']['rack_env'] = 'production'
+default['deployment']['revision'] = 'CURRENT'
+
+default['envbuilder']['base_dir'] = '/home/certificates'
+default['envbuilder']['base_dbi'] = 'production'
+default['envbuilder']['owner'] = 'certificates'
+default['envbuilder']['group'] = 'certificates'
+
+default['ruby-ng']['ruby_version'] = '1.9.3'
+default['ruby-ng']['dev_package'] = '1.9.1'
