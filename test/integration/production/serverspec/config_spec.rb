bb4aa6327fa91af0619702b306e6eca55d348350
pikesley <github@orgraphone.org>
2015-08-26 15:45:21 +0100

    File changed

--- test/integration/production/serverspec/config_spec.rb
+++ test/integration/production/serverspec/config_spec.rb
@@ -6,5 +6,5 @@ describe file '/home/certificates/certificates.theodi.org/shared/config/database
   its(:content) { should match /production:/ }
   its(:content) { should match /adapter: mysql2/ }
   its(:content) { should match /pool: 50/ }
-  its(:md5sum) { should eq 'db1eedc675035cfda73e8a56711b1ee0' }
+  its(:md5sum) { should eq 'eb4943bb770b84245567accda87a420a' }
 end
